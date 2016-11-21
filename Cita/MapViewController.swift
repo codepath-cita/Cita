4f0934cc0c290020c75eb04d4414469089123529
Santiago Gomez Lavin <gomezlavin@gmail.com>
2016-11-20 16:42:08 -0800

    localization works and translates geo to addresses

--- Cita/MapViewController.swift
+++ Cita/MapViewController.swift
@@ -11,34 +11,51 @@ import GoogleMaps
 
 class MapViewController: UIViewController {
     
-    @IBOutlet weak var mapView: UIView!
+    @IBOutlet weak var mapView: GMSMapView!
     @IBOutlet weak var tabBarView: UIView!
+    
+    var locationManager = CLLocationManager()
+    var didFindMyLocation = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.black
-        let frame = CGRect(x: view.frame.origin.x, y: view.frame.origin.y, width: view.frame.width, height: view.frame.height)
+//        view.backgroundColor = UIColor.black
+//        let frame = CGRect(x: view.frame.origin.x, y: view.frame.origin.y, width: view.frame.width, height: view.frame.height)
+//        
+//        let camera = GMSCameraPosition.camera(withLatitude: 37.77, longitude: -122.42, zoom: 15.0)
+//        let googleMapView = GMSMapView.map(withFrame: frame, camera: camera)
+//        googleMapView.isMyLocationEnabled = true
+//        mapView.addSubview(googleMapView)
+//        
+//        if let mylocation = googleMapView.myLocation {
+//            print("User's location: \(mylocation)")
+//        } else {
+//            print("User's location is unknown")
+//        }
+//        
+//        // Creates a marker in the center of the map.
+//        let marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2D(latitude: 37.77, longitude: -122.42)
+//        marker.title = "Here"
+//        marker.snippet = "you wish you were"
+//        marker.map = googleMapView
         
-        let camera = GMSCameraPosition.camera(withLatitude: 37.77, longitude: -122.42, zoom: 15.0)
-        let googleMapView = GMSMapView.map(withFrame: frame, camera: camera)
-        googleMapView.isMyLocationEnabled = true
-        mapView.addSubview(googleMapView)
-        
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: 37.77, longitude: -122.42)
-        marker.title = "Here"
-        marker.snippet = "you wish you were"
-        marker.map = googleMapView
 
-        seenError = false
-        locationFixAchieved = false
-        myLocationManager = CLLocationManager()
-        myLocationManager.delegate = self
-        myLocationManager.desiredAccuracy = kCLLocationAccuracyBest
-        myLocationManager.requestAlwaysAuthorization()
-        myLocationManager.startUpdatingLocation()
+//        seenError = false
+//        locationFixAchieved = false
+//        myLocationManager = CLLocationManager()
+//        myLocationManager.delegate = self
+//        myLocationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        myLocationManager.requestAlwaysAuthorization()
+//        myLocationManager.startUpdatingLocation()
+        
+        
+//        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: 48.857165, longitude: 2.354613, zoom: 8.0)
+//        mapView.camera = camera
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
         
     }
     
@@ -47,53 +64,42 @@ class MapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func reverseGeocodeCoordinate(coordinate: CLLocationCoordinate2D) {
+        let geocoder = GMSGeocoder()
+        
+        geocoder.reverseGeocodeCoordinate(coordinate) { response, error in
+            if let address = response?.firstResult() {
+                let lines = address.lines! as [String]
+                let addressString = lines.joined(separator: "\n")
+                print(addressString)
+            }
+        }
+    }
     
 }
+
 extension MapViewController: CLLocationManagerDelegate {
-    // Location Manager Delegate stuff
-    // If failed
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        myLocationManager.stopUpdatingLocation()
-        if (seenError == false) {
-            seenError = true
-            print(error)
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+            mapView.isMyLocationEnabled = true
+            mapView.settings.myLocationButton = true
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if (locationFixAchieved == false) {
-            locationFixAchieved = true
-            let locationArray = locations as NSArray
-            let locationObj = locationArray.lastObject as! CLLocation
-            let coord = locationObj.coordinate
-            
-            print("Latititude: \(coord.latitude)")
-            print("Longitude: \(coord.longitude)")
+        if let location = locations.first {
+            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+            locationManager.stopUpdatingLocation()
         }
-    }
-    
-    // authorization status
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        var shouldIAllow = false
         
-        switch status {
-        case CLAuthorizationStatus.restricted:
-            locationStatus = "Restricted Access to location"
-        case CLAuthorizationStatus.denied:
-            locationStatus = "User denied access to location"
-        case CLAuthorizationStatus.notDetermined:
-            locationStatus = "Status not determined"
-        default:
-            locationStatus = "Allowed to location Access"
-            shouldIAllow = true
-        }
-        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "LabelHasbeenUpdated"), object: nil)
-        if (shouldIAllow == true) {
-            NSLog("Location to Allowed")
-            // Start location services
-            myLocationManager.startUpdatingLocation()
-        } else {
-            NSLog("Denied access: \(locationStatus)")
-        }
     }
 }
+
+extension MapViewController: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
+        reverseGeocodeCoordinate(coordinate: position.target)
+    }
+}
+
+
