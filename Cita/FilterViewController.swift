3158b240a27fe5104adc1239d3bdd4ef2b09d303
Santiago Gomez Lavin <gomezlavin@gmail.com>
2016-11-27 15:35:08 -0800

    added filter screen modally

--- Cita/FilterViewController.swift
+++ Cita/FilterViewController.swift
@@ -21,6 +21,14 @@ class FilterViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onSearch(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     
     /*
      // MARK: - Navigation
